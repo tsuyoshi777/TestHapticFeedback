0a6f37fda768ee513dde9a0dfe24c4e95134e218
tsuyoshi777 <t12619tt@gmail.com>
2019-05-26 10:35:26 +0900

    Initial Commit

---
+++ TestHapticFeedback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHapticFeedback
+//
+//  Created by tuyoshi tonobe on 2019/05/26.
+//  Copyright © 2019 tuyoshi tonobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
